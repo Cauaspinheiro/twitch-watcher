6ba855a55e83b80b5ffb411929cca4d8dbc1464d
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-07-24 23:18:44 -0300

    refactor(types): improves typing in watcher

--- src/services/watch.ts
+++ src/services/watch.ts
@@ -1,8 +1,9 @@
 import watch from 'node-watch'
 import path from 'path'
 import fs from 'fs'
+import { Config } from '../types'
 
-type Fn = (json: JSON) => unknown
+type Fn = (json: Config) => void
 
 export default function listen(fn: Fn) : void {
   const configPath = path.resolve(__dirname, '..', '..', 'config.json')
---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export interface Config {
+  streamers: Array<string>,
+  twitch_id: string,
+  twitch_secret: string,
+  jwt: string
+}
+
+interface Streamer {
+  'user_name': string,
+}
+
+export type StreamerReq = {data: Array<Streamer> }
