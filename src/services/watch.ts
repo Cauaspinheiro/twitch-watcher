b63d477ad0677db5e0eadfd310724854b7d9dcf6
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-07-25 13:31:54 -0300

    feat: add dipose func and cross-env config

--- src/services/watch.ts
+++ src/services/watch.ts
@@ -1,24 +1,36 @@
 import watch from 'node-watch'
 import path from 'path'
 import fs from 'fs'
+import os from 'os'
 import { Config } from '../types'
 
-type Fn = (json: Config) => void
+type Fn = (json: Config) => () => Promise<void>
 
 export default function listen(fn: Fn) : void {
-  const configPath = path.resolve(__dirname, '..', '..', 'config.json')
+  let configPath : string
+
+  if (process.env.NODE_ENV === 'dev') {
+    configPath = path.resolve(__dirname, '..', '..', 'config.json')
+  } else {
+    configPath = path.resolve('/', 'users', os.userInfo().username,
+      'desktop', 'twitch.json')
+  }
+
+  let dispose : () => Promise<void>
 
   function readAndRun() {
     fs.readFile(configPath, (err, data) => {
       if (err) throw err
 
-      fn(JSON.parse(data as any))
+      dispose = fn(JSON.parse(data as any))
     })
   }
   readAndRun()
 
   watch(configPath, async (evt, name) => {
     if (evt === 'remove') return
+
+    await dispose()
 
     readAndRun()
   })
