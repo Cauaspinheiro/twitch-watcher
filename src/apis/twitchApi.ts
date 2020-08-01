0107e2cdb65d7b2c9fe0be724bb59da62063b44b
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-07-31 22:45:05 -0300

    feat(apis): add function to call twitch_api in /streams endpoint

---
+++ src/apis/twitchApi.ts
@@ -0,0 +1,30 @@
+import axios from 'axios'
+import useConfig from '../hooks/useConfig'
+
+interface StreamsReq {
+  data: {
+    user_name: string
+  }[]
+}
+
+export default async function getStreamers(streamers: string[]) : Promise<string[] | undefined> {
+  const config = useConfig()
+
+  const api = axios.create({ baseURL: 'https://api.twitch.tv/helix' })
+
+  api.defaults.headers.authorization = `Bearer ${config.twitch_api.jwt}`
+
+  api.defaults.headers['client-id'] = config.twitch_api.twitch_id
+
+  const query = `user_login=${streamers.join('&user_login=')}`
+
+  try {
+    const { data: res } = await api.get<StreamsReq>(`/streams?${query}`)
+
+    const usernames = res.data?.map((streamer) => streamer.user_name.toLowerCase())
+
+    return usernames
+  } catch (err) {
+    return err
+  }
+}
