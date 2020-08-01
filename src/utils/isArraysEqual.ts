90fa6d9beeef0076ebfefad0ee62b5f89ef600c6
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-07-31 22:24:14 -0300

    style(utils): remove unnecessary comments and enforce code style

--- src/utils/isArraysEqual.ts
+++ src/utils/isArraysEqual.ts
@@ -3,16 +3,12 @@ export default function isArraysEqual(a: Array<unknown>, b: Array<unknown>) : bo
   if (a == null || b == null) return false
   if (a.length !== b.length) return false
 
-  // If you don't care about the order of the elements inside
-  // the array, you should sort both arrays here.
-  // Please note that calling sort on an array will modify that array.
-  // you might want to clone your array first.
-
   a.sort()
   b.sort()
 
   for (let i = 0; i < a.length; i += 1) {
     if (a[i] !== b[i]) return false
   }
+  
   return true
 }
