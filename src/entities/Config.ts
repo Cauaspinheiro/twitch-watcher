110ff418f7c62137c9697be1b30348837eeb4ca2
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-07-31 22:28:15 -0300

    feat: add type for json config file

---
+++ src/entities/Config.ts
@@ -0,0 +1,21 @@
+// TODO: DOCUMENT EACH PROPERTY
+
+export interface IConfig {
+  streamers: Streamer[]
+  level: number
+  twitch_api: {
+    twitch_id: string
+    twitch_secret: string
+    jwt: string
+    repeatEachMs: number
+  }
+  deactivate: boolean
+  url: string
+  notify: boolean
+  shutDown: boolean
+}
+
+interface Streamer {
+  username: string
+  level: number
+}
