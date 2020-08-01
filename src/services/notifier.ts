ed480d09d175c548b52abeb13432ad0c36c4b4c9
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-07-31 22:45:59 -0300

    feat(services): add function to notify based on process.env

---
+++ src/services/notifier.ts
@@ -0,0 +1,36 @@
+import notifier from 'node-notifier'
+import path from 'path'
+import useConfig from '../hooks/useConfig'
+
+const windowsNotifier = new notifier.WindowsToaster()
+
+function timeout(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+export default function notify(title: string, message: string) : Promise<unknown> {
+  const config = useConfig()
+
+  const options = {
+    appID: 'Twitch Watcher',
+    icon: path.resolve(__dirname, '..', '..', 'twitch.png'),
+    sound: false,
+    wait: false,
+    title,
+    message,
+  }
+
+  if (config.notify === true) {
+    windowsNotifier.notify(options)
+
+    return timeout(1000)
+  }
+
+  if (config.notify === undefined && process.env.NODE_ENV !== 'dev') {
+    windowsNotifier.notify(options)
+
+    return timeout(1000)
+  }
+
+  return timeout(0)
+}
