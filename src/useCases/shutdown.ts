babc93cd97f45054efa4a4d82485e7f3a25e4de2
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-08-01 11:03:19 -0300

    feat(shutdown): write config file to turn off config.shutDown

--- src/useCases/shutdown.ts
+++ src/useCases/shutdown.ts
@@ -1,7 +1,19 @@
 import Powershell from 'node-powershell'
+import fs from 'fs'
+import useConfig from '../hooks/useConfig'
+import cache from '../services/nodeCache'
 
 export default async function shutdown() : Promise<void> {
+  const config = useConfig()
   const ps = new Powershell({})
+
+  delete config.shutDown
+
+  const configPath = cache.get<string>('configPath')
+
+  if (configPath) {
+    fs.writeFileSync(configPath, JSON.stringify(config, undefined, 1))
+  }
 
   await ps.addCommand('shutdown /s')
   await ps.invoke()
