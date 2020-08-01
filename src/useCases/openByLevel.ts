b459c4f7a4d4d41b8bae2da9deba868f865f5284
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-07-31 22:48:29 -0300

    feat(use_cases): add function to open twich by config level

---
+++ src/useCases/openByLevel.ts
@@ -0,0 +1,33 @@
+import useConfig from '../hooks/useConfig'
+import urlByLevel from '../static/urlByLevel'
+import openArray from './openArray'
+import openOnMulti from './openOnMulti'
+
+export default async function openByLevel(ids: string[],
+  openStreamers?: string[]) : Promise<void> {
+  const config = useConfig()
+
+  switch (config.level) {
+    case 0:
+      await openArray(ids, config.url || urlByLevel[0])
+      break
+    case 1:
+      if (config.url) {
+        await openArray(ids, config.url)
+      } else {
+        await openArray(ids, urlByLevel[1])
+        await openArray(ids, urlByLevel[0])
+      }
+      break
+    case 2:
+      if (config.url) {
+        await openArray(ids, config.url)
+      } else {
+        await openOnMulti(ids, openStreamers)
+      }
+      break
+    default:
+      await openArray(ids, config.url || urlByLevel[3])
+      break
+  }
+}
