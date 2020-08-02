926d13db9ab7abbe659fdcaf1eede669382ff003
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-08-01 23:49:42 -0300

    feat(static/url): opens twitch.tv in theatre mode

--- src/static/urlByLevel.ts
+++ src/static/urlByLevel.ts
@@ -1,5 +1,5 @@
 export default {
   0: 'https://www.twitch.tv/popout/${streamer}/chat?popout=true',
   1: 'https://player.twitch.tv/?channel=${streamer}&enableExtensions=true&muted=true&parent=twitch.tv&player=popout',
-  3: 'https://www.twitch.tv/${streamer}',
+  3: 'https://www.twitch.tv/${streamer}?mode=theatre',
 }
